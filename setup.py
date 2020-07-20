892542cd420a114fad7bb09fdd19ba9329d2f3d4
Ali <61859768+treehousekingcomic@users.noreply.github.com>
2020-07-20 17:20:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,11 @@ setup(
   author = 'TreeHouseKingComic Software | Ali S.',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/treehousekingcomic/DiscordEmojiHandler',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/treehousekingcomic/DiscordEmojiHandler/archive/v1.0.tar.gz',    # I explain this later on
   keywords = ['discord.py', 'emojis', 'handler'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  install_requires=[],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
