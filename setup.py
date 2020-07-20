d1d3c84a85b2f6a027b21cd5ba066df03633bd4e
Ali <61859768+treehousekingcomic@users.noreply.github.com>
2020-07-20 17:31:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'TreeHouseKingComic Software | Ali S.',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/treehousekingcomic/DiscordEmojiHandler',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/treehousekingcomic/DiscordEmojiHandler/archive/v1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/treehousekingcomic/DiscordEmojiHandler/archive/v1.1.tar.gz',    # I explain this later on
   keywords = ['discord.py', 'emojis', 'handler'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
