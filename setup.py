cfe6664a21c00b5a3dee5921284d4da3bfd5bc25
Ali <61859768+treehousekingcomic@users.noreply.github.com>
2020-07-21 12:31:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'DiscordEmojiHandler',         # How you named your package folder (MyLib)
   packages = ['DiscordEmojiHandler'],   # Chose the same as "name"
-  version = '1.5',      # Start with a small number and increase it with every change you make
+  version = '1.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = "Work with messages that contain emojis that your bot can't see? Emoji Handler will take care of the problem.",   # Give a short description about your library
   author = 'TreeHouseKingComic Software | Ali S.',                   # Type in your name
   author_email = 'discordemojihandler@thkc.space',      # Type in your E-Mail
   url = 'https://github.com/treehousekingcomic/DiscordEmojiHandler',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/treehousekingcomic/DiscordEmojiHandler/archive/v1.5.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/treehousekingcomic/DiscordEmojiHandler/archive/v1.6.tar.gz',    # I explain this later on
   keywords = ['discord.py', 'emojis', 'handler'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
