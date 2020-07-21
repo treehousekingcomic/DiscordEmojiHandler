89a64ba2140b79550bde0b2ee3efe2fe623bf7a2
Ali <61859768+treehousekingcomic@users.noreply.github.com>
2020-07-21 19:07:34 -0400

    Update for v1.6

--- DiscordEmojiHandler/EmojiHandlerModule.py
+++ DiscordEmojiHandler/EmojiHandlerModule.py
@@ -2,17 +2,15 @@ import re
 
 class EmojiHandler():
     # regex module required (import re)
-    def __init__(self, bot, content: str):
+    def __init__(self, bot):
         # bot can be discord.Client or discord.ext.Bot
-        # content is the text to check
         self.bot = bot
-        self.content = content
         self.emoji_regex = r'<(?P<animated>a?):(?P<name>[a-zA-Z0-9_]{2,32}):(?P<id>[0-9]{18,22})>'
-        self.emojis = re.findall(self.emoji_regex, self.content)
 
-    def check(self):
+    def check(self, content):
         # Returns True if all emojis in the text are available to the bot, False if not.
-        for result in self.emojis:
+        emojis = re.findall(self.emoji_regex, content)
+        for result in emojis:
             id_ = int(result[2])
 
             emoji = self.bot.get_emoji(id_)
@@ -24,30 +22,35 @@ class EmojiHandler():
                 continue
         return True
 
-    def fix(self, colon=False):
+    def fix(self, content, colon=False):
         # Returns original content with the emojis that the bot can't see replaced with their name only.
         # If colon is True, invalid emojis are replaced with :name: (with the :)
-        for result in self.emojis:
+        if not content:
+            return None
+        emojis = re.findall(self.emoji_regex, content)
+        for result in emojis:
             id_, name_, animated_ = result
 
             emoji = self.bot.get_emoji(id_)
             if not emoji or not emoji.is_usable():
-                self.content = self.content.replace(f'<{animated_}:{name_}:{id}>', f'{name if not colon else ":" + name_ + ":"}')
+                content = content.replace(f'<{animated_}:{name_}:{id}>', f'{name if not colon else ":" + name_ + ":"}')
             else:
                 continue
-        return self.content
+        return content
 
-    def replace(self, text='🤔', invalid_only=True):
+    def replace(self, content, text='🤔', invalid_only=True):
         # Returns the original content with invalid emojis replaced with text (default: 🤔)
         # If you want to replace all emojis, pass False to invalid_only
+        if not content:
+            return None
         for result in self.emojis:
             id_, name_, animated_ = result
 
             emoji = self.bot.get_emoji(id_)
             if not emoji or not emoji.is_usable():
-                self.content = self.content.replace(f'<{animated_}:{name_}:{id}>', text)
+                content = content.replace(f'<{animated_}:{name_}:{id}>', text)
             elif not invalid_only:
-                    self.content = self.content.replace(f'<{animated_}:{name_}:{id}>', text)
+                    content = content.replace(f'<{animated_}:{name_}:{id}>', text)
             else:
                 continue
-        return self.content
+        return content
