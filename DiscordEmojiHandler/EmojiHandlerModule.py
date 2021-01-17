9d6508d6b271c779ebfeea13348084c6cfb52b89
Md Shahriyar Alam <40076722+shahprog@users.noreply.github.com>
2021-01-18 01:01:15 +0600

    Docstring and argument types
    Prettify things

--- DiscordEmojiHandler/EmojiHandlerModule.py
+++ DiscordEmojiHandler/EmojiHandlerModule.py
@@ -1,15 +1,23 @@
 import re
+import discord
 
 class EmojiHandler():
-    # regex module required (import re)
-    def __init__(self, bot):
-        # bot can be discord.Client or discord.ext.Bot
+    """The emoji handler class
+    
+    Parameters:
+        bot (typing.Union[discord.Bot, discord.AutoShardedBot, discord.Client])
+    """
+    def __init__(self, bot: typing.Union[discord.commands.Bot, discord.commands.AutoShardedBot, discord.Client]):
         self.bot = bot
         self.emoji_regex = r'<(?P<animated>a?):(?P<name>[a-zA-Z0-9_]{2,32}):(?P<id>[0-9]{18,22})>'
 
-    def check(self, content):
-        # Returns True if all emojis in the text are available to the bot, False if not.
-        emojis = re.findall(self.emoji_regex, content)
+    def check(self, content: str):
+        """Returns True if all emojis in the text are available to the bot, False if not
+        
+        Parameters:
+            content (str): The message content
+        """
+        emojis = re.findall(self.emoji_regex, content: str)
         for result in emojis:
             id_ = int(result[2])
 
@@ -22,12 +30,18 @@ class EmojiHandler():
                 continue
         return True
 
-    def fix(self, content, colon=False):
-        # Returns original content with the emojis that the bot can't see replaced with their name only.
-        # If colon is True, invalid emojis are replaced with :name: (with the :)
+    def fix(self, content: str, colon: bool=False):
+        """
+        Returns original content with the emojis that the bot can't see replaced with their name only.
+        If colon is True, invalid emojis are replaced with :name: (with the :)
+        
+        Parameters:
+            content (str): The message content
+            colon (bool): Whether to replace with :name: or not
+        """
         if not content:
             return None
-        emojis = re.findall(self.emoji_regex, content)
+        emojis = re.findall(self.emoji_regex, content: str)
         for result in emojis:
             animated_, name_, id_ = result
 
@@ -38,9 +52,18 @@ class EmojiHandler():
                 continue
         return content
 
-    def replace(self, content, text='🤔', invalid_only=True):
-        # Returns the original content with invalid emojis replaced with text (default: 🤔)
-        # If you want to replace all emojis pass False to invalid_only
+    def replace(self, content: str, text: str ='🤔', invalid_only: bool=True):
+        """Returns the original content with invalid emojis replaced with text (default: 🤔)
+        If you want to replace all emojis pass False to invalid_only
+        
+        Parameters:
+            content (str): The message content
+            text (str): What to replace with
+            invalid_only (bool): Whether to replace all emojis or not
+        
+        Returns: 
+            content (str): The replaced content/message string
+        """
         if not content:
             return None
         emojis = re.findall(self.emoji_regex, content)
