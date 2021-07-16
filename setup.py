815d63eab60d0d74a87f92b72acc49392f82715b
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2021-07-16 14:28:04 +0600

    fixes a typo in setup.py
    changed pyhton to python

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "Development Status :: 4 - Beta",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Intended Audience :: Developers",  # Define that your audience are developers
         "License :: OSI Approved :: MIT License",  # Again, pick a license
-        "Programming Language :: Python :: 3",  # Specify which pyhton versions that you want to support
+        "Programming Language :: Python :: 3",  # Specify which python versions that you want to support
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
