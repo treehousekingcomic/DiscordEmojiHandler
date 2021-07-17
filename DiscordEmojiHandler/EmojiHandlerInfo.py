f53fc0822dc5a0b054c4a78269c3e1b79b8f3e5b
Ali <61859768+treehousekingcomic@users.noreply.github.com>
2021-07-16 23:24:49 -0400

    v1.8

--- DiscordEmojiHandler/EmojiHandlerInfo.py
+++ DiscordEmojiHandler/EmojiHandlerInfo.py
@@ -1,7 +1,7 @@
 """DiscordEmojiHandler Info."""
 
-version = 1.7
-__version_notes__ = "All previous versions (below v1.7) do not work."  # Some notes
+version = 1.8
+__version_notes__ = "Version 1.8 better formats the library for a production package with docstrings and some documentation made by Wasi Master. No breaking changes from v1.7."  # Some notes
 
 __rewrite__ = "DiscordEmojiHandler v1.6"  # Version 1.6 was the rewrite of DiscordEmojiHandler, you now only need to make a EmojiHandler instance per bot and you pass your content in the functions.
 __rewrite_version__ = 1.6
